c3021a1d10e4496180691b028bf6870fe72b746e
KFB7NFJ <7777888@ups.com>
2022-10-27 21:19:36 -0400

    Mocked API

--- TestGetRepos.py
+++ TestGetRepos.py
@@ -1,4 +1,8 @@
+import json
 import unittest
+import unittest.mock
+from unittest.mock import patch
+from unittest.mock import Mock
 
 from GetRepos import fetchRepos
 
@@ -12,7 +16,16 @@ class TestHW04GetRepos(unittest.TestCase):
     def testAPIB(self):
         self.assertEqual(fetchRepos('marishadhimar'), ['Repo: helloworld  Number of commits: 2', 'Repo: HW_04A_GitHubApi  Number of commits: 13', 'Repo: portfolio  Number of commits: 1', 'Repo: Simon-Game  Number of commits: 1', 'Repo: SSW_567_HW01  Number of commits: 14', 'Repo: SSW_567_HW02_A  Number of commits: 2', 'Repo: story  Number of commits: 2'])
 
+    @patch("GetRepos.fetchRepos", ['Repo: helloworld  Number of commits: 2', 'Repo: HW_04A_GitHubApi  Number of commits: 13', 'Repo: portfolio  Number of commits: 1', 'Repo: Simon-Game  Number of commits: 1', 'Repo: SSW_567_HW01  Number of commits: 14', 'Repo: SSW_567_HW02_A  Number of commits: 2', 'Repo: story  Number of commits: 2'])
+    def testMockAPI(self):
+        mockResponseFile = open('MockResponse.json')
+        jsonResponse = json.load(mockResponseFile)
+        mockResponseFile.return_value = Mock(200)
+        mockResponseFile.return_value.json = jsonResponse
+        response = fetchRepos("marishadhimar")
+        self.assertEqual(response[0], 'Repo: helloworld  Number of commits: 2')
+        mockResponseFile.close()
+
 
 if __name__ == '__main__':
-    print('Running unit tests')
     unittest.main()
